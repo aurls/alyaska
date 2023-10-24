04c9fd0e417c47338983c2168d8b375e6b38d945
Vladislav Taranov <vladislav.taranov@live.com>
2023-10-24 18:31:09 +0300

    initial commit

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, make_response, jsonify
+
+PORT = 9100
+
+app = Flask(__name__)
+
+def createResponse(data):
+  response = make_response(data)
+
+  response.headers['X-Frame-Options'] = 'DENY'
+  response.headers['X-XSS-Protection'] = '0'
+  response.headers['X-Content-Type-Options'] = 'nosniff'
+  response.headers['Referrer-Policy'] = 'strict-origin-when-cross-origin'
+  response.headers['Set-Cookie'] = 'SameSite=Strict; Secure; HttpOnly'
+  response.headers['X-Powered-By'] = 'alyaska'
+  response.headers['Server'] = 'alyaska'
+
+  return response
+
+@app.route('/api/v1/attrition', methods=['POST'])
+def hello():
+  try:
+    params = request.json.get('params')
+
+    # run model here like getPrediction(params)
+    prediction = 666
+
+  except:
+    return createResponse(jsonify(status='error'))
+
+  else:
+    return createResponse(jsonify(status='success', value=prediction))
+
+app.run(debug=True, port=PORT)
